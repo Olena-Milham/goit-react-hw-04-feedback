16c8f58acb4a172b74baa6c77c5b41c2a2d130c0
Olena-Milham <nikimilh@gmail.com>
2022-08-25 23:22:49 +0100

    change to hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,104 @@
-
-import {FeedbackOptions} from 'components/FeedbackOptions';
+import { FeedbackOptions } from 'components/FeedbackOptions';
 import { Statistics } from 'components/Statistics';
 import { Box } from 'components/UI/Box';
-import {Component} from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
 
-class App extends Component{
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
+  const stateOptions = { good, neutral, bad };
 
-  handleLeaveFeedback =(option)=>{
-    this.setState(prevState => {
-    return {[option]:prevState[option] +1};
-    })
-  }
+  let total = good + neutral + bad;
+  let percentage = total !== 0 ? Math.round((good * 100) / total) : 0;
 
-  countTotalFeedback = () => {
-    const  { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+  const handleLeaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
-
-  countPositiveFeedbackPercentage=()=>{
-    const total=this.countTotalFeedback();
-    const {good}=this.state;
-    const positiveFeedback=total !== 0 ? Math.round((good * 100) / total) : 0;
-    return positiveFeedback
-}
-
-  render (){
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const percentage=this.countPositiveFeedbackPercentage()
-    const options =Object.keys(this.state)
-    return (
-      <Box as='main' pl={5} pt={5} width='50%'> 
-      <FeedbackOptions options={options} onLeaveFeedback={this.handleLeaveFeedback}/>
-      <Statistics 
-         good={good}
-         neutral={neutral}
-         bad={bad}
-         total={total}
-         positivePercentage={percentage}
+  return (
+    <Box as="main" pl={5} pt={5} width="50%">
+      <FeedbackOptions
+        options={Object.keys(stateOptions)}
+        onLeaveFeedback={handleLeaveFeedback}
       />
-      </Box>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        positivePercentage={percentage}
+      />
+    </Box>
   );
 }
-};
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
 
-export default App;
+//   handleLeaveFeedback = option => {
+//     this.setState(prevState => {
+//       return { [option]: prevState[option] + 1 };
+//     });
+//   };
 
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
 
+//   countPositiveFeedbackPercentage = () => {
+//     const total = this.countTotalFeedback();
+//     const { good } = this.state;
+//     const positiveFeedback = total !== 0 ? Math.round((good * 100) / total) : 0;
+//     return positiveFeedback;
+//   };
 
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = this.countTotalFeedback();
+//     const percentage = this.countPositiveFeedbackPercentage();
+//     const options = Object.keys(this.state);
+//     return (
+//       <Box as="main" pl={5} pt={5} width="50%">
+//         <FeedbackOptions
+//           options={options}
+//           onLeaveFeedback={this.handleLeaveFeedback}
+//         />
+//         <Statistics
+//           good={good}
+//           neutral={neutral}
+//           bad={bad}
+//           total={total}
+//           positivePercentage={percentage}
+//         />
+//       </Box>
+//     );
+//   }
+// }
 
+// export default App;
 
-// ===== when there are a lot of items 
-  // countTotalFeedback=()=>{
-  //   let total = Object.values(this.state).reduce((acc, value) => {
-  //     return acc + value;
-  //   }, 0);
-  //   return total;
-  // }
-
+// ===== when there are a lot of items
+// countTotalFeedback=()=>{
+//   let total = Object.values(this.state).reduce((acc, value) => {
+//     return acc + value;
+//   }, 0);
+//   return total;
+// }
